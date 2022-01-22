18a55f967f361a284b6cfb306e306e36809fe4e2
remmi755 <90457721+remmi755@users.noreply.github.com>
2022-01-22 20:59:46 +0300

    add changes

--- scripts.js
+++ scripts.js
@@ -1,13 +1,14 @@
 $('.test').css('color', 'red')
 $('h2.test').css('color', 'red')
-$('p').nextAll('p').css('color', 'red')
-$('li.www').nextAll('li').css('color', 'red')
+$('p').next('p').css('color', 'red')
+$('li.www').next('li').css('color', 'red')
 $('h2').find('i').css('color', 'red')
 $('h2').children('i').css('color', 'red')
 $('h2').not('.test').css('color', 'red')
 $(':header').not('.test').css('color', 'red')
-$(':header').nextAll(':header').css('color', 'red')
-$(':header.test').nextAll(':header.test').css('color', 'red')
+$(':header').next(':header').css('color', 'red')
+$(':header.test').next(':header.test').css('color', 'red')
+
 
 
 
