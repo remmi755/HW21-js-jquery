0bb615442ed98c6d18911eadc6f06b426b866f81
remmi755 <90457721+remmi755@users.noreply.github.com>
2022-01-23 12:26:43 +0300

    adds

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 $('.test').css('color', 'red')
 $('h2.test').css('color', 'red')
-$('p').next('p').css('color', 'red')
-$('li.www').next('li').css('color', 'red')
+$('p + p').css('color', 'red')
+$('li.www + li').css('color', 'red')
 $('h2').find('i').css('color', 'red')
 $('h2').children('i').css('color', 'red')
 $('h2').not('.test').css('color', 'red')
 $(':header').not('.test').css('color', 'red')
-$(':header').next(':header').css('color', 'red')
-$(':header.test').next(':header.test').css('color', 'red')
+$(':header + :header').css('color', 'red')
+$(':header.test + :header.test').css('color', 'red')
 
 
 
